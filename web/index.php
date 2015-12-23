e979437f11f35945efb29edb371da3492b05d167
vatseek <vadim.gumennyj@gmail.com>
2015-12-23 22:05:11 +0200

    add ace editor

--- web/index.php
+++ web/index.php
@@ -2,27 +2,32 @@
 <html>
 <head>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            background-color: #333;
-        }
-    </style>
+    <link rel="stylesheet" href="css/styles.css">
     <script src="./js/pixi.js"></script>
     <title>Bomberman game</title>
 </head>
 <body>
+<div>
 
+</div>
 <div id="editor">
-    function foo(items) {
+function foo(items) {
     var x = "All this is syntax highlighted";
     return x;
-    }
+}
 </div>
+
 
 <script src="https://code.jquery.com/jquery-2.1.4.min.js" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
+
+<script src="/ace-builds/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
+<script>
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/dawn");
+    editor.getSession().setMode("ace/mode/javascript");
+</script>
+
 <script src="./js/app.js"></script>
 </body>
 </html>
